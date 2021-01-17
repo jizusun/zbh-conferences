2302e9d50e211bc97b1fe37b159e5bae13f19941
Jizu Sun <jizu.sun@sap.com>
2021-01-17 17:43:14 +0800

    add comment

--- hugo-ifaise2020/httrack/run.sh
+++ hugo-ifaise2020/httrack/run.sh
@@ -4,8 +4,12 @@ httrack https://ifaise2020.edtechstar.com/en/ https://ifaise2020.edtechstar.com/
     -B -T1 -v -N3
     # "%h/%p-%n.%t"
 
+
+#        -B     can both go up&down into the directory structure (--can-go-up-and-down)
+#        -T1    timeout 1s
+#        -v     verbose
 #        -N3    all files in web/
+
 #        -n     get non-html files  near  an html file (ex: an image located outside) (--near) 
 #        -d     stay on the same principal domain (--stay-on-same-domain)
 #        -iC    update without confirmation
-#        -T1    timeout 1s
